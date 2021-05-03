98a4cace13d085c12c174917e8b1438cb0029b27
Ocean-9598 <barusagar007@gmail.com>
2021-05-03 13:12:59 -0500

    git staging

--- index.js
+++ index.js
@@ -13,9 +13,6 @@ const PORT = process.env.PORT || 5000;
 const server = new ApolloServer({
     cors: {
         origin: "*",
-        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-        preflightContinue: false,
-        optionsSuccessStatus: 204,
         credentials: true
       },
       playground:true,
